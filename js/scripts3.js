0ff0c1b1949f67cd95a9505042125fd1f136f5dd
duncan19 <duncanrobbins19@gmail.com>
2020-03-16 10:11:31 -0700

    add more equations

--- js/scripts3.js
+++ js/scripts3.js
@@ -2,3 +2,31 @@ function subFunc() {
   var sub = 5 - 2;
   document.getElementById("math").innerHTML = "5 - 2 = " + sub;
 }
+function mulFunc() {
+  var mul = 5 * 2;
+  document.getElementById("math").innerHTML = "5 * 2 = " + mul;
+}
+function divFunc() {
+  var div = 5 / 2;
+  document.getElementById("math").innerHTML = "5 / 2 = " + div;
+}
+function subAddFunc() {
+  var subAdd = 5 - 2 * 3;
+  document.getElementById("math").innerHTML = "5 - 2 = " + subAdd;
+}
+function remFunc() {
+  var rem = 5 % 2 ;
+  document.getElementById("math").innerHTML = "5 % 2 = " + rem;
+}
+function mynum() {
+  var x = 234
+  document.getElementById("math").innerHTML = -x;
+}
+function mynum2() {
+  var y = 234
+  document.getElementById("math").innerHTML = --y;
+}
+function ranNum {
+  var num = (Math.random() * 100)
+  alert(num)
+}
