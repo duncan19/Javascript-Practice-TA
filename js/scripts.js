9561de41f7a0f0d11ac45c9af6eda5dbaee9d0af
duncan19 <duncanrobbins19@gmail.com>
2020-03-16 11:18:24 -0700

    fix errors inside of index.html and srcipts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,9 @@ var blue = blue.fontcolor("blue")
 document.write(blue)
 alert("hello")
 document.write(3 + 3);
+var myString = "Sam's unbrella is yellow"
+var a = 5, b = 2 , c = 3
+console.log(a);
+console.log(b);
+console.log(c);
+document.write (myString + " " + blue)
