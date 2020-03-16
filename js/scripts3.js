6f42aeb1d4a2a32757734743564edf53be527bc5
duncan19 <duncanrobbins19@gmail.com>
2020-03-16 11:24:05 -0700

    add html for the 3rd practice

--- js/scripts3.js
+++ js/scripts3.js
@@ -4,29 +4,29 @@ function subFunc() {
 }
 function mulFunc() {
   var mul = 5 * 2;
-  document.getElementById("math").innerHTML = "5 * 2 = " + mul;
+  document.getElementById("math2").innerHTML = "5 * 2 = " + mul;
 }
 function divFunc() {
   var div = 5 / 2;
-  document.getElementById("math").innerHTML = "5 / 2 = " + div;
+  document.getElementById("math3").innerHTML = "5 / 2 = " + div;
 }
 function subAddFunc() {
   var subAdd = 5 - 2 * 3;
-  document.getElementById("math").innerHTML = "5 - 2 = " + subAdd;
+  document.getElementById("math4").innerHTML = "5 - 2 = " + subAdd;
 }
 function remFunc() {
   var rem = 5 % 2 ;
-  document.getElementById("math").innerHTML = "5 % 2 = " + rem;
+  document.getElementById("math5").innerHTML = "5 % 2 = " + rem;
 }
 function mynum() {
   var x = 234
-  document.getElementById("math").innerHTML = -x;
+  document.getElementById("math6").innerHTML = -x;
 }
 function mynum2() {
   var y = 234
-  document.getElementById("math").innerHTML = --y;
+  document.getElementById("math7").innerHTML = --y;
 }
-function ranNum {
+function ranNum() {
   var num = (Math.random() * 100)
   alert(num)
 }
